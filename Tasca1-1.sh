6db6b00b19735ebe35d20c4cb2a1441d66ab4a60
Nat <NataliaCortesD@autonoma.cat>
2022-12-18 12:46:20 +0100

    tasca1-1 en proces

--- Tasca1-1.sh
+++ Tasca1-1.sh
@@ -2,4 +2,27 @@
 
 fitxer=$1
 
-echo |`sort -k3 -d;` $fitxer
+sort -k3 -t";" $fitxer > aux.t
+lin= wc -l < aux.t
+
+for i in `seq 1 1 $lin`
+do 
+	echo *********************************
+	titol= `cut -d";" -f3 aux.t|head -$i|tail -1`
+	echo -e "Títol: $titol" 
+	any= `cut -d";" -f1 aux.t|head -$i| tail -1`
+	long= `cut -d";" -f2 aux.t|head -$i|tail -1`
+	pop= `cut -d";" -f8 aux.t| head -$i|tail -1`
+	premis= `cut -d";" -f9 aux.t|head -$i| tail -1`
+	tema= `cut -d";" -f4 aux.t|head -$i|tail-1`
+	echo -e "Any: $any	Longitud: $long	Popularitat: $pop	Premis: $premis	Tema: $tema"
+	actor= `cut -d";" -f5 aux.t|head -$i|tail -1`
+	echo -e "Actor: $actor"
+	actriu= `cut -d";" -f6 aux.t|head -$i|tail -1`
+	echo -e "Actriu: $actriu"
+	echo **********************************
+done
+
+rm aux.t
+
+#echo $(comando)
