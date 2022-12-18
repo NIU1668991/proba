c32758d1ce1def636807e85f6b02deb29d815a3b
Nat <NataliaCortesD@autonoma.cat>
2022-12-18 14:16:34 +0100

    tasca1-2 en proces

--- Tasca1-2.sh
+++ Tasca1-2.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+fitxer=$1
+
+sort $fitxer > aux.t
+lin=`wc -l < aux.t`
+let lin=lin-3
+echo $lin
+
+head -$lin aux.t > aux2.t
+
+i=1
+while [ $i -le $lin ]
+do
+	echo "**************************************"
+	titol=$(cut -f3 -d";"|head -$i|tail -1)
+	echo $titol
+
+done
+echo "**************************************"
+
+cat aux2.t
+
+rm aux.t aux2.t
+
