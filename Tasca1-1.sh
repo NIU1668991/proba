6ec5e5a7d915a43a05ea2a7d7f821adb03081bb8
Nat <NataliaCortesD@autonoma.cat>
2022-12-18 13:31:13 +0100

    tasca1-1 funiconant i bon format

--- Tasca1-1.sh
+++ Tasca1-1.sh
@@ -3,10 +3,11 @@
 fitxer=$1
 
 sort -k3 -t";" $fitxer > aux.t
-lin= wc -l < aux.t
+lin=`wc -l < aux.t`
 
-for i in `seq 1 $lin 1`
-do 
+i=3
+while [ $i -le $lin ]  
+do
 	echo "*********************************"
 	echo -e "Títol:" `cut -d";" -f3 aux.t|head -$i|tail -1`
 	echo -e "Any: `cut -d";" -f1 aux.t|head -$i| tail -1`	Longitud: `cut -d";" -f2 aux.t|head -$i|tail -1`	Popularitat: `cut -d";" -f8 aux.t| head -$i|tail -1`	Premis: `cut -d";" -f9 aux.t|head -$i| tail -1`	Tema: `cut -d";" -f4 aux.t|head -$i|tail -1`"
