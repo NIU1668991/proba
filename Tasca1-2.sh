9ce96bf98bbc8586c1cc92047b227c6aba309133
Nat <NataliaCortesD@autonoma.cat>
2022-12-18 16:41:23 +0100

    Tasca1-2 feta i funcionant

--- Tasca1-2.sh
+++ Tasca1-2.sh
@@ -13,13 +13,21 @@ i=1
 while [ $i -le $lin ]
 do
 	echo "**************************************"
-	titol=$(cut -f3 -d";"|head -$i|tail -1)
-	echo $titol
-
+	titol=$(cut -f3 -d";" aux2.t|head -$i|tail -1)
+	echo "Titol: $titol"
+	any=$(cut -f1 -d";" aux2.t|head -$i|tail -1)
+	long=$(cut -f2 -d";" aux2.t|head -$i|tail -1)
+	pop=$(cut -f8 -d";" aux2.t|head -$i|tail -1)
+	premis=$(cut -f9 -d";" aux2.t|head -$i|tail -1)
+	tema=$(cut -f4 -d";" aux2.t|head -$i|tail -1)
+	echo "Any: $any	Longitud: $long	Popularitat: $pop	Premis: $premis	Tema: $tema"
+	actor=$(cut -f5 -d";" aux2.t|head -$i|tail -1)
+	echo "Actor: $actor"
+	actriu=$(cut -f6 -d";" aux2.t|head -$i|tail -1)
+	echo "Actriu: $actriu"
+	let i=i+1
 done
 echo "**************************************"
-
-cat aux2.t
-
 rm aux.t aux2.t
+
 
