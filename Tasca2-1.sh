7bf2b62412ad45bfb19cb508dc534e72d654773b
Nat <NataliaCortesD@autonoma.cat>
2022-12-19 18:29:03 +0100

    tasca2-1 en proces

--- Tasca2-1.sh
+++ Tasca2-1.sh
@@ -8,10 +8,11 @@ read titol
 cut -f3 -d";" $fitxer|grep -i $titol|sort -k3 -t";"> aux
 lin=`wc -l < aux`
 
+grep aux $fitxer > aux2
 
 if [ $lin -ne 0 ];
 then
-	cat aux
+	cat aux2
 else 
 	echo "No hi ha cap coincidencia"
 fi
