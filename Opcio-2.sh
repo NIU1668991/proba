4607f501f34c1d4b5f80ee8af2e305e3286baf3f
Nat <NataliaCortesD@autonoma.cat>
2022-12-19 19:09:50 +0100

    tot al dia

---
+++ Opcio-2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "*****************************************"
+echo "Cerca de pel·lícula"
+echo "1 - Cercar pel·lículas per títol"
+echo "2 - Cercar pel·lícules fetes entre dos anys"
+echo "3 - Cercar pel·lícules on ha intervingut una determinada persona"
+echo "0 - Tornar al menú anterior"
--- Tasca2-1.sh
+++ Tasca2-1.sh
@@ -12,11 +12,29 @@ grep -f aux $fitxer > aux2
 
 if [ $lin -ne 0 ];
 then
-	cat aux2
+	i=1
+	while [ $i -le $lin ]
+	do
+	echo "*************************************"
+	titol=$(cut -d";" -f3 aux2|head -$i|tail -1)
+	echo "Títol: $titol"
+	any=$(cut -d";" -f1 aux2|head -$i|tail -1)
+	long=$(cut -d";" -f2 aux2|head -$i|tail -1)
+	pop=$(cut -d";" -f8 aux2|head -$i|tail -1)
+	premis=$(cut -d";" -f9 aux2|head -$i|tail -1)
+	tema=$(cut -d";" -f4 aux2|head -$i|tail -1)
+	echo "Any: $any	Longitud: $long	Popularitat: $pop	Premis: $premis	Tema: $tema"
+	actor=$(cut -d";" -f5 aux2|head -$i|tail -1)
+	echo "Actor: $actor"
+	actriu=$(cut -d";" -f6 aux2|head -$i|tail -1)
+	echo "Actriu: $actriu"
+	let i=i+1
+	done
+echo "************************************"
 else 
-	echo "No hi ha cap coincidencia"
+	echo "No hi ha cap coincidencia"	
 fi
 
-
+rm aux aux2 
 
 #-f ARCHIVO extrae los patrones del archivo que especifiquemos. Los patrones del archivo deben ir uno por línea
